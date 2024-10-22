faec2256c54b2e6cb9cd8c58bc550fe819d88712
Om Chavan <omchavan7512@gmail.com>
2024-10-22 19:19:43 +0530

    new test cases added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, add, markAsComplete } = todoList();
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
 describe("Todo List Test Suite", () => {
   beforeAll(() => {
@@ -20,11 +20,26 @@ describe("Todo List Test Suite", () => {
         .slice(0, 10),
       completed: false,
     });
-    expect(all.length).toBe(todoItemCount+1);
+    expect(all.length).toBe(todoItemCount + 1);
   });
   test("Mark a todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Retrieval of overdue items", () => {
+    let items = overdue();
+    if (items.length === 0) expect(items.length).toBe(0);
+    else expect(items.length).toBeGreaterThan(0);
+  });
+  test("Retrieval of today due items", () => {
+    let items = dueToday();
+    if (items.length === 0) expect(items.length).toBe(0);
+    else expect(items.length).toBeGreaterThan(0);
+  });
+  test("Retrieval of due later items", () => {
+    let items = dueLater();
+    if (items.length === 0) expect(items.length).toBe(0);
+    else expect(items.length).toBeGreaterThan(0);
   });
 });
